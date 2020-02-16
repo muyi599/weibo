cb9f38ef88769a4bd36d2177faa3a30496bf62a1
Leo Yang <muyi599@qq.com>
2020-02-16 15:42:39 +0800

    基础页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home(){
+        return view("StaticPages/home");
+    }
+
+    public function help()
+    {
+        return view('StaticPages/help');
+    }
+
+    public function about()
+    {
+        return view('StaticPages/about');
+    }
+}
